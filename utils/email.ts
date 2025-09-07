1fa6f14a4b94def595a41d28fa9580aaf34fe0c6
Biplov Thapa <biplovthapa@Biplovs-MacBook-Pro.local>
2025-09-07 12:40:33 +0545

    added email configuration

--- utils/email.ts
+++ utils/email.ts
@@ -30,7 +30,7 @@ export const sendEmail = async ({ email, emailType, userId }: EmailOptions) => {
     const verifyUrl=`${process.env.FRONTEND_URL}/verifyemail?token=${hashedToken}&userId=${userId}`
     console.log(process.env.FRONTEND_URL)
     const mailResponse = await transporter.sendMail({
-        from:"Devforces",
+        from:"biplovthapa456@gmail.com",
         to:email,
         subject:emailType=="VERIFY"?"verify your email":"",
         html:`<p>Click here to verify your email <a href="${verifyUrl}">here</a>
